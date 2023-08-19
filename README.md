79932a41b1b299141aed331a4c2e48854dbacde8
Austin Hunt <austinsketches@gmail.com>
2023-08-19 15:54:19 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# "Login Required Simple Plugin" for WordPress
+This is a super simple WordPress plugin tha defines a custom shortcode `[login_required]` that you can add to posts or pages in order to check if a user is logged in before letting them view the content. If they are not, it redirects them to the login screen. 
---
+++ login-required.php
@@ -0,0 +1,13 @@
+<?php 
+/*
+ * Plugin Name: Login Required Simple Plugin
+ * Plugin URI: https://github.com/austinjhunt/wp-login-required-plugin
+ * Description: enable this plugin to allow use of a [login_required] shortcode on pages that require a login in order to view them
+ */
+add_shortcode('login_required', 'shortcode_loginRequired');
+function shortcode_loginRequired() {
+    if (!is_user_logged_in()) {
+        auth_redirect();
+    }
+}
+`
